cc157c386a62bf159a3ae21f55fafe78b8b41308
QobiljonYuldashev <qobiljonyuldashev044>
2025-09-15 13:18:26 +0500

    done projects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ const AppRouter = () => {
           children: [
             { path: "/", element: <Home /> },
             { path: "shop", element: <Shop /> },
-            { path: "shop/:id", element: <ProductDetail /> },
+            { path: "products/:id", element: <ProductDetail /> },
           ],
         },
         // private route
